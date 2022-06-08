b7a64089dc78ad5327b03679713c13c4eee806eb
Bennyboy <bgbennyboy@users.noreply.github.com>
2022-06-08 23:29:05 +0100

    More detection of version from scripts
    Refactored detection
    Support more resource files with different xor values

--- formMain.pas
+++ formMain.pas
@@ -6,7 +6,8 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
   Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Grids,
   System.ImageList, Vcl.ImgList,  Vcl.ExtCtrls, IOUtils, System.Hash, IdHashCRC,
-  AdvMemo, AdvObj, AdvGrid, AdvUtil, BaseGrid, System.RegularExpressions,
+  AdvMemo, AdvObj, AdvGrid, AdvUtil, BaseGrid, System.RegularExpressions,  System.Character,
+  System.StrUtils,
   JCLFileUtils, JCLShell, JclStrings,
   OtlTask, OtlCollections, OtlParallel, OtlSync, tmsAdvGridExcel,
   uMemReader;
@@ -36,12 +37,26 @@ type
     ExeFiles: TStringList;
     function IsExeInvalid(OutputText: string): boolean;
     function ExtractStringFromResourceFiles(Path, Exename: string): string;
+    function SearchStreamForValidVersionString(TheStream: TExplorerMemoryStream; XORVal: Byte): string;
     procedure GetFileChecksums(TheFile: string; var CRC: string; var MD5: string);
     procedure Log(LogItem: String);
   public
     { Public declarations }
   end;
 
+const
+  ExesToExclude: array[0..52] of string =
+    ('8514HERE.EXE','8514HERE.EXE','ACROREAD.EXE','ACRTEST.EXE', 'ALLBOOT.EXE',
+    'AR16D30.EXE','AR32D30.EXE','AR32D301.EXE','AR500DEU.EXE','ASSAULT.EXE',
+    'AT.EXE','BINMOD.EXE','BOOTDISK.EXE','BOOTDSK2.EXE','BOOTFULL.EXE',
+    'BOOTMKR.EXE','CDCOPY.EXE','CDPLAY.EXE','CHECKCD.EXE','CONTROL.EXE',
+    'DARK.EXE','DDHELP.EXE','DIGSTART.EXE','DOS4GW.EXE','DOTT.EXE',
+    'DPLAYSVR.EXE','DXINFO.EXE','DXSETUP.EXE','DXTOOL.EXE','GRIMOVIE.EXE',
+    'HELPME.EXE','IMUSE.EXE','INSTALIT.EXE','INSTALL.EXE','INSTDX50.EXE',
+    'INSTI4CD.EXE','LHA.EXE','LHA213.EXE','MOUSECHK.EXE','NECGMMUT.EXE',
+    'RBL2DEMO.EXE','README.EXE','REBEL.EXE','REBELSE.EXE','REGISTER.EXE',
+    'RUN386.EXE','SDTCT.EXE','SDTCT.EXE','SETMUSE.EXE','SETUP.EXE',
+    'T_OAK2.EXE','WEBSITE.EXE','_ISDEL.EXE');
 var
   frmMain: TfrmMain;
   loop : IOmniParallelLoop<integer>;
@@ -51,7 +66,7 @@ implementation
 
 {$R *.dfm}
 
-function StripNonAscii(const s: string): string;
+{function StripNonAscii(const s: string): string;
 var
   i, Count: Integer;
 begin
@@ -64,6 +79,23 @@ begin
     end;
   end;
   SetLength(Result, Count);
+end;}
+
+function GetAlphaSubstr2(const Str: string): string;
+var
+  ActualLength: integer;
+  i: Integer;
+begin
+  SetLength(result, length(Str));
+  ActualLength := 0;
+  for i := 1 to length(Str) do
+    //if TCharHelper.IsLetter(Str[i]) then
+    if Str[i].IsLetterOrDigit or Str[i].IsWhiteSpace or Str[i].IsPunctuation then
+    begin
+      inc(ActualLength);
+      result[ActualLength] := Str[i];
+    end;
+  SetLength(Result, ActualLength);
 end;
 
 function FindFileHeader(SearchStream: TExplorerMemoryStream;
@@ -231,6 +263,7 @@ begin
     begin
       log('Scanning finished.');
       log('Raw outputs can be found at ' + ExtractFilePath(Application.ExeName) + 'outputs\');
+      AdvStringGrid1.ColumnSize.StretchColumn := AdvStringGrid1.ColCount; //make last column the stretch one
       beep;
       loop := nil;
       cancelToken.Clear;
@@ -260,6 +293,17 @@ begin
             OutText := Trim( TFile.ReadAllText('outputs\out' + inttostr(value) + '.txt') );  //Read the output
             AdvStringGrid1.AllCells[8, value+1] := OutText;
 
+            //Ignore this exe if in ignore array
+            if MatchText(extractfilename(ExeFiles[value]), ExesToExclude) then
+            begin
+              for i := 0 to (AdvStringGrid1.ColCount -1) do
+                AdvStringGrid1.Colors[i, value+1] := clGreen;//$003743ED;
+
+              exit;
+            end;
+
+
+            //Parse resource files and try and extract version+date from the scripts
             resfilestring := ExtractStringFromResourceFiles( ExtractFilePath(ExeFiles[value]), extractfilename(ExeFiles[value]));
             AdvStringGrid1.AllCells[7, value+1] := resfilestring;
 
@@ -298,7 +342,6 @@ begin
           AdvStringGrid1.AllCells[5, value+1] := CRC32;
           AdvStringGrid1.AllCells[6, value+1] := MD5;
 
-          AdvStringGrid1.ColumnSize.StretchColumn := AdvStringGrid1.ColCount; //make last column the stretch one
           AdvStringGrid1.AutoSizeRows(false, 0); //Resize the rows
         end
       );
@@ -318,7 +361,7 @@ end;
 
 function TfrmMain.ExtractStringFromResourceFiles(Path, Exename: string): string;
 const
-  ResourceFileExtensions: array [0..1] of string = ('.LA0', '.LEC');
+  ResourceFileExtensions: array [0..3] of string = ('.LA0', '.LEC', '.LFL', '.001');
 var
   i, j, k, foundoffset, blocksize: integer;
   MemStream: TExplorerMemoryStream;
@@ -365,14 +408,15 @@ begin
         MemStream.Clear;
         MemStream.LoadFromFile(Path + FoundFiles[j]);
 
-        if ResourceFileExtensions[i] = '.LA0' then
+        {if ResourceFileExtensions[i] = '.LA0' then
           MemStream.SetXORVal($0)
         else
-          MemStream.SetXORVal($69);
+          MemStream.SetXORVal($69);}
 
         //>=V7 Full Throttle, The Dig, CMI
         if ResourceFileExtensions[i] = '.LA0' then
         begin
+          MemStream.SetXORVal($0);
           foundoffset := FindFileHeader(memstream, 0, memstream.Size, 'MAXS');
           if foundoffset > -1 then
           begin
@@ -384,7 +428,17 @@ begin
         end;
 
         //DISK.LEC and .LFL Files. Search the entire file for strings.
-        foundoffset := 0;
+        Result := SearchStreamForValidVersionString(MemStream, $69);
+        if Result = '' then
+          Result := SearchStreamForValidVersionString(MemStream, $FF); //Last crusade ega uses 0xFF
+        if Result = '' then
+          Result := SearchStreamForValidVersionString(MemStream, $0); //Last crusade VGA is not xor'ed
+
+        if Result <> '' then exit;
+
+
+        //DISK.LEC and .LFL Files. Search the entire file for strings.
+        {foundoffset := 0;
         while foundoffset <> -1 do
         begin
           foundoffset := FindFileHeader(MemStream, foundoffset, MemStream.Size, #$27#$01); //These hex values preceed strings in the scripts
@@ -396,16 +450,52 @@ begin
           end;
         end;
 
-        //Now do regex to get the string we want. Search for date strings.
-        for k := 0 to FoundStrings.Count -1 do
-        begin
-          if TRegEx.IsMatch(FoundStrings[k], '[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}', [roNone]) then
+        //Last crusade uses 0xFF so if nothing has been found try with FF
+        if FoundStrings.Count = 0 then
+        begin                                    //Last crusade 76.lfl
+          MemStream.SetXORVal($FF);
+          foundoffset := 0;
+          while foundoffset <> -1 do
           begin
-            result := StripNonAscii(FoundStrings[k]);
-            log(result);
-            Exit;
+            foundoffset := FindFileHeader(MemStream, foundoffset, MemStream.Size, #$27#$01); //These hex values preceed strings in the scripts
+            if foundoffset > 0 then
+            begin
+              MemStream.Position := foundoffset;
+              FoundStrings.Add(MemStream.ReadNullTerminatedString(100));
+              foundoffset := MemStream.Position; //Update pointer with where we are after reading the string
+            end;
           end;
         end;
+
+        //Last crusade VGA is not xor'ed
+        if FoundStrings.Count = 0 then
+        begin
+          MemStream.SetXORVal($00);
+          foundoffset := 0;
+          while foundoffset <> -1 do
+          begin
+            foundoffset := FindFileHeader(MemStream, foundoffset, MemStream.Size, #$27#$01); //These hex values preceed strings in the scripts
+            if foundoffset > 0 then
+            begin
+              MemStream.Position := foundoffset;
+              FoundStrings.Add(MemStream.ReadNullTerminatedString(100));
+              foundoffset := MemStream.Position; //Update pointer with where we are after reading the string
+            end;
+          end;
+        end; }
+
+        //Now do regex to get the string we want. Search for date strings.     //[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}'
+        //for k := 0 to FoundStrings.Count -1 do
+        //begin
+        //  if TRegEx.IsMatch(FoundStrings[k], '[0-3]?[0-9].[0-3]?[0-9][\.\/-](?:[0-9]{2})?[0-9]{2}', [roNone]) then
+        //  begin
+       //     result := GetAlphaSubstr2(FoundStrings[k]);
+       //     if result[1] = '''' then  //First character of many is '
+       //       delete(result, 1, 1);
+            //log(result);
+       //     Exit;
+       //   end;
+       // end;
       end;
     end;
   finally
@@ -471,5 +561,46 @@ end;
 
 
 
+
+function TfrmMain.SearchStreamForValidVersionString(
+  TheStream: TExplorerMemoryStream; XORVal: Byte): string;
+var
+  foundoffset, i: integer;
+  FoundStrings: TStringList;
+begin
+  result := '';
+  TheStream.SetXORVal(XORVal);
+  foundoffset := 0;
+
+  FoundStrings := TStringList.Create;
+  try
+    while foundoffset <> -1 do
+    begin
+      foundoffset := FindFileHeader(TheStream, foundoffset, TheStream.Size, #$27#$01); //These hex values preceed strings in the scripts
+      if foundoffset > 0 then
+      begin
+        TheStream.Position := foundoffset;
+        FoundStrings.Add(TheStream.ReadNullTerminatedString(100));
+        foundoffset := TheStream.Position; //Update pointer with where we are after reading the string
+      end;
+    end;
+
+    //Now do regex to get the string we want. Search for date strings.     //[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}'
+    for i := 0 to FoundStrings.Count -1 do
+    begin
+      if TRegEx.IsMatch(FoundStrings[i], '[0-3]?[0-9].[0-3]?[0-9][\.\/-](?:[0-9]{2})?[0-9]{2}', [roNone]) then
+      begin
+        result := GetAlphaSubstr2(FoundStrings[i]);
+        if result[1] = '''' then  //First character of many is '
+          delete(result, 1, 1);
+        //log(result);
+        Exit;
+      end;
+    end;
+  finally
+    FoundStrings.Free;
+  end;
+
+end;
 
 end.
